543e9fdcf6ca6fff874fa6fff7547f2eb455f8f4
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-11-20 22:08:00 +0700

    Separated dataFilter

--- src/DataTable.js
+++ src/DataTable.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 class DataTable extends Component {
+	dataFilter = (item) => (item.major === this.props.major);
   render() {
     return (
 			<div>
-				<BootstrapTable data={this.props.data.filter((item) => item.major === this.props.major)} bordered>
-					<TableHeaderColumn dataField='interviewRef' width="200">Ref. No</TableHeaderColumn>
-					<TableHeaderColumn dataField='firstName' width="200" isKey>First Name</TableHeaderColumn>
+				<BootstrapTable data={this.props.data.filter(this.dataFilter)} bordered striped hover>
+					<TableHeaderColumn dataField='interviewRef' width="200" isKey>Ref. No</TableHeaderColumn>
+					<TableHeaderColumn dataField='firstName' width="200">First Name</TableHeaderColumn>
 					<TableHeaderColumn dataField='lastName' width="200">Last Name</TableHeaderColumn>
-					<TableHeaderColumn dataField='major' width="200">Major</TableHeaderColumn>
+					{/* <TableHeaderColumn dataField='major' width="200">Major</TableHeaderColumn> */}
 				</BootstrapTable>
 			</div>
     );
