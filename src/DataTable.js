38ddf79ae5561ac5d753712b7d8b1e99bb0a2b5a
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-11-21 10:15:40 +0700

    Added sort options, formatted name

--- src/DataTable.js
+++ src/DataTable.js
@@ -3,13 +3,19 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 class DataTable extends Component {
 	dataFilter = (item) => (item.major === this.props.major && (item.firstName.includes(this.props.keyword) || item.lastName.includes(this.props.keyword)));
-  render() {
+	sortOptions = {
+		defaultSortName: 'interviewRef',
+		defaultSortOrder: 'asc',
+		sortIndicator: false,
+		noDataText: 'ไม่พบชื่อที่ค้นหา'
+	};
+	nameFormatter = (cell, row) => (row.firstName + ' ' + row.lastName);
+	render() {
     return (
 			<div>
-				<BootstrapTable data={this.props.data.filter(this.dataFilter)} bordered striped hover>
-					<TableHeaderColumn dataField='interviewRef' width="200" isKey>Ref. No</TableHeaderColumn>
-					<TableHeaderColumn dataField='firstName' width="200">First Name</TableHeaderColumn>
-					<TableHeaderColumn dataField='lastName' width="200">Last Name</TableHeaderColumn>
+				<BootstrapTable data={this.props.data.filter(this.dataFilter)} options={this.sortOptions} bordered striped hover>
+					<TableHeaderColumn dataField='interviewRef' width="200" isKey dataSort>Ref. No</TableHeaderColumn>
+					<TableHeaderColumn dataField='firstName' width="200" dataFormat={this.nameFormatter}>Name</TableHeaderColumn>
 				</BootstrapTable>
 			</div>
     );
