65c059ce3095edbddef5edafe182bdfb999b37ae
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-11-22 17:41:45 +0700

    Removed fluid from Navbar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, FormGroup, FormControl } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, FormGroup, FormControl, NavDropdown, MenuItem } from 'react-bootstrap';
 
 class NavigationBar extends Component {
 	render() {
 		return (
-			<Navbar fixedTop inverse fluid>
+			<Navbar fixedTop inverse collapseOnSelect>
 				<Navbar.Header pullLeft>
 					<Navbar.Brand>
 						YWC15 Candidates
 					</Navbar.Brand>
 				</Navbar.Header>
-				<Nav bsStyle="pills" activeKey={this.props.major} onSelect={this.props.handleSelect} pullLeft>
-					{this.props.majors.map((x) => (<NavItem eventKey={x}>{x}</NavItem>))}
-				</Nav>
-				<Navbar.Form pullLeft>
-					<FormGroup>
-						<FormControl type="text" placeholder="Search" onChange={this.props.handleSearch}/>
-					</FormGroup>
-				</Navbar.Form>
-				<Navbar.Link pullRight>
-					<span><a href='https://ywc15.ywc.in.th' target='_blank'>Enter Site</a></span>
-				</Navbar.Link>
+				<Navbar.Collapse>
+					<Nav bsStyle="pills" activeKey={this.props.major} onSelect={this.props.handleSelect} pullLeft>
+						{this.props.majors.map((x) => (<NavItem eventKey={x}>{x.charAt(0).toUpperCase() + x.slice(1)}</NavItem>))}
+					</Nav>
+					<Navbar.Form pullLeft>
+						<FormGroup>
+							<FormControl type="text" placeholder="Search" onChange={this.props.handleSearch}/>
+						</FormGroup>
+					</Navbar.Form>
+					<Navbar.Text pullRight>
+						<Navbar.Link href='https://ywc15.ywc.in.th' target='_blank'>Enter YWC Site</Navbar.Link>
+					</Navbar.Text>
+				</Navbar.Collapse>
 			</Navbar>
 		);
 	}
