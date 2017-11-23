e2b2c1f19e22ea27ae703acdd6f425c4bfbc49be
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-11-23 23:25:29 +0700

    Started on Grid

--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import DataTable from './DataTable.js';
-import { Grid, Row } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Body extends Component {
 	render() {
 		return <div style={{minHeight: window.innerHeight}}>
-        {/* <Grid>
-          <Row>
-
-          </Row>
-        </Grid> */}
-        <DataTable data={this.props.data} major={this.props.major} keyword={this.props.keyword}/>
+        <Grid>
+          <Col xs={7}>
+            <DataTable data={this.props.data} major={this.props.major} keyword={this.props.keyword}/>
+          </Col>
+        </Grid>
 		</div>
 	}
 }
