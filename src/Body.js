22fb33c37136d53f32eade5de7f135fb9be15395
Attawit Chaiyaroj <ahm.913@hotmail.com>
2017-11-25 17:12:36 +0700

    Added assignments for real

--- src/Body.js
+++ src/Body.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import DataTable from './DataTable.js';
 import SideTable from './SideTable.js';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col, PageHeader, Panel } from 'react-bootstrap';
 
 class Body extends Component {
 	render() {
 		return <div style={{minHeight: window.innerHeight}}>
         <Grid>
-          <Col xs={12} md={6} style={{backgroundColor: 'white'}}>
-            <DataTable data={this.props.data} major={this.props.major} keyword={this.props.keyword}/>
+          <Col xs={12} md={7}>
+            <Panel>
+              <PageHeader>ประกาศรายชื่อผู้มีสิทธิ์เข้าสัมภาษณ์ สาขา {this.props.major.charAt(0).toUpperCase() + this.props.major.slice(1)}</PageHeader>
+              <DataTable data={this.props.data} major={this.props.major} keyword={this.props.keyword}/>
+            </Panel>
           </Col>
-          {/* <Col xs={4} xsPush={1} style={{backgroundColor: 'white'}}>
+          <Col xs={4} xsPush={1}>
             <SideTable major={this.props.major}/>
-          </Col> */}
+          </Col>
           
         </Grid>
 		</div>
--- src/SideTable.js
+++ src/SideTable.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const assignments = {
-	content: "ให้น้อง ๆ เลือกทำคอนเทนต์ใด คอนเทนต์หนึ่งจาก 2 หัวข้อด้านล่าง โดยต้องทำเป็นคอนเทนต์ออกมาจริงเท่านั้น ไม่จำกัดรูปแบบการนำเสนอ<br><p>หัวข้อที่ให้เลือก</p><br>1. คอนเทนต์ที่ทำให้ผู้ใช้ Social เข้าใจ Cyberbullying และตระหนักถึงปัญหาที่เกิดขึ้นจาก Cyberbullying<br>2. คอนเทนต์ที่ทำให้คนไทยเปลี่ยนพฤติกรรมเสพติดหน้าจอ เสพติด Social หันมาพูดคุยกัน เจอหน้ากันในชีวิตจริง ๆ มากกว่าที่เป็นอยู่"
-
+	content: <span>
+			<p>ให้น้อง ๆ เลือกทำคอนเทนต์ใด คอนเทนต์หนึ่งจาก 2 หัวข้อด้านล่าง โดยต้องทำเป็นคอนเทนต์ออกมาจริงเท่านั้น ไม่จำกัดรูปแบบการนำเสนอ</p>
+			<p><b>หัวข้อที่ให้เลือก</b></p>
+			<p>1. คอนเทนต์ที่ทำให้ผู้ใช้ Social เข้าใจ Cyberbullying และตระหนักถึงปัญหาที่เกิดขึ้นจาก Cyberbullying</p>
+			<p>2. คอนเทนต์ที่ทำให้คนไทยเปลี่ยนพฤติกรรมเสพติดหน้าจอ เสพติด Social หันมาพูดคุยกัน เจอหน้ากันในชีวิตจริง ๆ มากกว่าที่เป็นอยู่</p>
+		</span>,
+	design: <span>
+			<p>ให้ผู้ผ่านเข้ารอบสัมภาษณ์ออกแบบเว็บไซต์ของ <b>"คณะรักษาความสงบแห่งชาติ หรือ คสช."</b> เพื่อเป็นสื่อกลางในการประชาสัมพันธ์นโยบายต่าง ๆ และเป็นสื่อกลางระหว่างประชาชนกับคณะรักษาความสงบแห่งชาติ โดยให้ออกแบบในลักษณะของ One Page Design Website</p>
+			<p>** สำหรับผู้ผ่านเข้ารอบสัมภาษณ์คนใดทำ CSS มา คณะกรรมการจะพิจารณาเป็นพิเศษ</p>
+		</span>,
+	marketing: <span>
+			<p>ให้ทำแผนการตลาด (Marketing Plan) สำหรับเว็บไซต์หนึ่งเว็บไซต์ โดยเลือกเว็บไซต์ใด ๆ ก็ได้ ทั้งเว็บไซต์ของไทยและเว็บไซต์ของต่างประเทศ</p>
+			<p>นำเสนอในรูปแบบพรีเซนเทชั่น <b>ไม่เกิน 10 หน้า ภายในเวลา 5 นาที</b></p>
+		</span>,
+	programming: <span>
+			<p>ให้เขียน <b>เว็บไซต์ประกาศผลผู้ผ่านเข้ารอบสัมภาษณ์ของ YWC#15</b> โดยใช้ข้อมูลจาก <a href='https://ywc15.ywc.in.th/api/interview' target='_blank'>API</a> โดยมี Feature ดังนี้</p>
+			<ul>
+				<li>ดึงข้อมูลจาก API โดยตรง</li>
+				<li>ให้แสดงผลแยกแต่ละสาขา</li>
+				<li>มีระบบค้นหาชื่อผ่านกล่อง Search</li>
+				<li>ความสามารถหรือ Feature พิเศษอื่น ๆ ที่มีความแตกต่าง และแสดงความสามารถของน้องออกมาให้ได้มากที่สุด</li>
+			</ul>
+			<p>เมื่อทำเสร็จแล้วให้ Push Source Code ขึ้น GitHub ก่อนเวลาสัมภาษณ์</p>
+		</span>
 };
 
 class SideTable extends Component {
 	render() {
 		return <div>
-        <Grid>
-					<Row>
-						
-					</Row>
-					<Row>
-						{assignments[this.props.major]}
-					</Row>
-				</Grid>
+			  <ListGroup>
+					<ListGroupItem header={"การบ้านสาขา " + this.props.major.charAt(0).toUpperCase() + this.props.major.slice(1)}>{assignments[this.props.major]}</ListGroupItem>
+					{/* <ListGroupItem header="Heading 2" href="#">Linked item</ListGroupItem>
+					<ListGroupItem header="Heading 3" bsStyle="danger">Danger styling</ListGroupItem> */}
+				</ListGroup>
 		</div>
 	}
 }
